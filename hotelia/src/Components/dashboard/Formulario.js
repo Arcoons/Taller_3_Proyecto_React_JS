336222be035cf088c655c3e22aae28231da78167
Christian <christianzaor1090@gmail.com>
2022-09-30 00:17:59 -0500

    Css

--- hotelia/src/Components/dashboard/Formulario.js
+++ hotelia/src/Components/dashboard/Formulario.js
@@ -1,28 +1,29 @@
+import p2 from "./p2.jpeg"
 import '../../pages/dasboard/dashboard.css'
 
-function Formulario({usuario, imagen}) {
+function Formulario() {
       return(
     <section className="datos">
-                  <div className="formulario">
+                  <div className="formularioDash">
                         <div className="tituloFormulario">
-                              <h1>Bienvenido {usuario}</h1> 
+                              <h1>Bienvenido Usuario</h1> 
                         </div>
                         <div className="info">
                               <div className="logo-img">
-                                    <img src={imagen}/>
+                                    <img src={p2}/>
                                     <div className="carga">
                                           <div id="log-des"><i className="fa-solid fa-file-arrow-up"></i></div>
                                           <div id="log1-des"><i className="fa-solid fa-upload"></i></div>
                                     </div>
                                     <button>Cambiar Contraseña</button>
                               </div>
-                              <div className="form">
+                              <div className="formDash">
                                     <div className="docDash">
-                                          <div id="inf">
+                                          <div id="infDash">
                                                 <label for="tip-doc">Tipo de documento</label>
                                                 <input type="text" name="tip-doc" placeholder="CC" className="inputsDash"  />
                                           </div>
-                                          <div id="inf">
+                                          <div id="infDah">
                                                 <label for="num-doc">Número documento</label>
                                                 <input type="text" name="num-doc" placeholder="10000000" className="inputsDash" />
 
@@ -31,11 +32,11 @@ function Formulario({usuario, imagen}) {
                                           
                                     </div>
                                     <div className="nomDash">
-                                          <div id="inf">
+                                          <div id="infDash">
                                                 <label for="nombre">Nombres</label>
                                                 <input type="text" name="nombre" placeholder="Nombre.." className="inputsDash"/>
                                           </div>
-                                          <div id="inf">
+                                          <div id="infDash">
                                                 <label for="Apellido">Apellidos</label>
                                                 <input type="text" name="Apellido" placeholder="Apellidos.." className="inputsDash"/>
 
@@ -43,26 +44,26 @@ function Formulario({usuario, imagen}) {
                                     </div>
                                     <div className="origen">
 
-                                          <div id="inf">
+                                          <div id="infDash">
                                                 <label for="fecha">Fecha nacimiento</label>
                                                 <input type="text" name="fecha" placeholder="DD/MM/YY" className="inputsDash"/>
                                           </div>
-                                          <div id="inf">
+                                          <div id="infDash">
                                                 <label for="origen">País de origen</label>
                                                 <input type="text" name="origen" placeholder="País.." className="inputsDash"/>
 
                                           </div>
                                     </div>
                                     <div className="genero">
-                                          <div id="inf">
-                                                <div className="radio">
+                                          <div id="infDash">
+                                                <div className="radioDash">
                                                 <label for="fecha">Género</label>
                                                 <input type="radio" name="genero" className="inputsDash"/>Mujer
                                                 <input type="radio" name="genero" className="inputsDash"/>Hombre
                                                 <input type="radio" name="genero" className="inputsDash"/>Otro
                                                 </div>
                                           </div>
-                                          <div id="inf">
+                                          <div id="infDash">
                                                 <label for="tel">Teléfono de contacto</label>
                                                 <input type="text" name="tel" placeholder="Teléfono"className="inputsDash" />
 
@@ -76,11 +77,11 @@ function Formulario({usuario, imagen}) {
 
                                     </div>
                                     <div className="contraDash">
-                                          <div id="inf">
+                                          <div id="infDash">
                                                 <label for="password">Contraseña</label>
                                                 <input type="password" name="password" className="inputsDash"/>
                                           </div>
-                                          <div id="inf">
+                                          <div id="infDash">
                                                 <label for="passwordd">Confirmar contraseña</label>
                                                 <input type="password" name="passwordd" className="inputsDash"/>
 
