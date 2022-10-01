7476faa2b540bffbcab447105808dbc0cbda3d81
ranyerino1 <jetacuma3@misena.edu.co>
2022-09-30 22:39:39 -0500

    Props y Error

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import ReactDOM from 'react-dom/client';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Registro from './pages/registro/Registro';
+import Login from './pages/login/Login.js';
+import Index from './pages/index/index';
+import Dashboard from './pages/dasboard/dashboard';
+
+
+
+import './index.css';
+
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <Routes>
+    <Route path='/' element={<Index />}></Route>
+    <Route path='/dashboard' element={<Dashboard/>}></Route>
+    <Route path='/login' element={<Login />}></Route>
+    <Route path='/registro' element={<Registro />}></Route>
+    </Routes>
+  </BrowserRouter>
+  
+    
+  
+);
+
+
